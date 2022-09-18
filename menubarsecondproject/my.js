98ee08e6fe818cc77544863ae464e0b4973f8fef
pratibha rajawat <preetu098@gmail.com>
2022-09-18 14:56:33 +0530

    add changes

---
+++ menubarsecondproject/my.js
@@ -0,0 +1,11 @@
+function load()
+{
+    if(document.getElementById('mobile').style.display=='none')
+    {
+        document.getElementById('mobile').style.display='block';  
+    }
+    else
+    {
+        document.getElementById('mobile').style.display='none';
+    }
+}
